6fb31f3161c71cd4c045d5c7585da92e66dcc5a5
Carol H <sheseee@gmail.com>
2018-12-22 00:52:35 +0800

    [feature/get-payload] mockup reply_worker

--- app/controllers/listener/lines_controller.rb
+++ app/controllers/listener/lines_controller.rb
@@ -1,5 +1,20 @@
 class Listener::LinesController < ApplicationController
+  before_action :events, only: [:check]
+
   def check
+    @events.each do |event|
+      reply_token = event['replyToken']
+      rumor       = event['message']['text']
+
+      ReplyWorker.perform_async(reply_token, rumor)
+    end
+
     head :ok
+  end
+
+  private
+
+  def events
+    @events = params['events']
   end
 end
