2adb23b8229c91b97010ebbc057acecd0f9c96d0
Carol H <sheseee@gmail.com>
2018-12-27 02:29:20 +0900

    [feature/introduction] fix

--- app/lib/intro.rb
+++ app/lib/intro.rb
@@ -17,11 +17,18 @@ class Intro
     end
 
     def introduction
-      <<~INTRO
+      text = <<~INTRO
       早安您好～我是美玉姨!
-      我會聽大家說的各種話並即時查詢假消息，
+      阿姨喜歡聽大家說的各種話並即時查詢假消息，
+      我會提供多元看法幫助各位多想一下。
+      至於大家曾經講過什麼，阿姨記性不好，過目即忘～
       很開心認識新朋友 <3
       INTRO
+
+      {
+        type: 'text',
+        text: text
+      }
     end
   end
 end
