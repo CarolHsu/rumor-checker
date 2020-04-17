a8c182d65525baa53e16353fc050727fea6f512b
Carol H <sheseee@gmail.com>
2020-04-17 17:04:30 -0400

    [feature/telegram] apply to reply_worker

--- app/controllers/listener/telegrams_controller.rb
+++ app/controllers/listener/telegrams_controller.rb
@@ -1,5 +1,18 @@
 class Listener::TelegramsController < ApplicationController
+  before_action :get_message, only: [:check]
+
   def check
+    reply_token = @message[:chat][:id]
+    rumor = @message[:text]
+
+    ReplyWorker.perform_async(reply_token, rumor, 'telegram')
+
     head :ok
+  end
+
+  private
+
+  def get_message
+    @message = params[:message]
   end
 end
