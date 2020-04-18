0f4dc18895d1efbd99206cb2db8a052bba46677d
Carol H <sheseee@gmail.com>
2020-04-18 18:12:06 -0400

    [feat/covid-19-telegram] refactoring

--- app/controllers/listener/telegrams_controller.rb
+++ app/controllers/listener/telegrams_controller.rb
@@ -1,22 +1,27 @@
 class Listener::TelegramsController < ApplicationController
   before_action :get_message, only: [:check]
+  before_action :get_params, only: [:check]
 
   def check
-    reply_token = @message[:chat][:id]
-    # TODO: (carol) apply CoronavirusReplyWorker
-    # chat_type = @message[:chat][:type]
-    rumor = @message[:text]
-
-    return unless forwardable?(rumor)
-
-    ReplyWorker.perform_async(reply_token, rumor, 'telegram')
+    check_rumor
 
     head :ok
   end
 
   private
 
+  def check_rumor
+    return unless forwardable?(@rumor)
+    ReplyWorker.perform_async(@reply_token, @rumor, 'telegram')
+  end
+
   def get_message
     @message = params[:message]
+  end
+
+  def get_params
+    @reply_token = @message[:chat][:id]
+    @chat_type   = @message[:chat][:type]
+    @rumor       = @message[:text]
   end
 end
