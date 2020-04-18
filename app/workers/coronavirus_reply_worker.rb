b3fa10b6b262371a9d825af36d4fa61b4cd807cf
Carol H <sheseee@gmail.com>
2020-04-18 11:37:17 -0400

    [feat/covid-19] add coronavirus_reply_worker

---
+++ app/workers/coronavirus_reply_worker.rb
@@ -0,0 +1,56 @@
+require 'line/bot'
+
+class CoronavirusReplyWorker
+  include Sidekiq::Worker
+  sidekiq_options retry: false
+
+  def perform(token, rumor, platform='line')
+    @token = token
+    @rumor = rumor
+    @platform = platform
+
+    answer_for_nonsense_query
+    answer_for_out_of_service_query unless @answer
+    answer_for_query unless @answer
+
+    talk(@answer)
+  end
+
+  private
+
+  def answer_for_nonsense_query
+    @rumor = begin
+               Integer(@rumor)
+             rescue
+               nil
+             end
+
+    return if @rumor
+    @answer = '抱歉，這超出我的回答範圍囉。'
+  end
+
+  def answer_for_out_of_service_query
+    # TODO: build config/coronavirus.yml
+  end
+
+  def answer_for_query
+    # TODO: build config/coronavirus.yml
+  end
+
+  def talk(reply)
+    case @platform
+    when 'line'
+      client = initiate_client
+      client.reply_message(@token, reply)
+    when 'telegram'
+      HTTParty.post(
+        "https://api.telegram.org/bot#{ENV['telegram_app_token']}/sendMessage",
+        headers: { "Content-Type": "application/json"},
+        body: {
+          chat_id: @token,
+          text: reply
+        }
+      )
+    end
+  end
+end
